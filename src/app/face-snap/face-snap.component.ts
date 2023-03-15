8bd890fbd73b82e2768cbc900eada970550c2c8e
Adama55 <mafahfof55@gmail.com>
2023-03-15 22:38:46 +0100

    affihcer les donnees

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
-
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  creatDate!:Date;
+  snaps!: number;
+  imagUrl!:string;
+  
+  ngOnInit(){
+    this.title ='Zaban';
+    this.description ='fruit saisonier de de l afrique de louest ';
+    this.creatDate= new Date() ;
+    this.snaps = 6;
+    this.imagUrl="https://www.webonary.org/dioula-bf/wp-content/blogs.dir/339/files/images/original/saba%20senegalensis.jpg"
+  }
 }
